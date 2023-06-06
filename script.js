cca945032986c6beb2f4f4bbd6bd6a39770273a7
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-06-06 17:52:33 -0500

    Created person class

--- script.js
+++ script.js
@@ -30,3 +30,68 @@ class Hamster {
         return this.price
     }
 }
+
+//=================================
+// Person
+// attributes:
+
+// name - set name from parameter in constructor method
+// age - initially 0
+// height - initially 0
+// weight - initially 0
+// mood - integer starting at 0 initially
+// hamsters - empty array initially
+// bankAccount - initially set to 0
+// methods:
+// getName() - returns name
+// getAge() - returns age
+// getWeight() - returns weight
+// greet() - logs a message with person's name
+// eat() - increment weight, increment mood
+// exercise() - decrement weight
+// ageUp() - increment age, increment height, increment weight, decrement mood, increment bank account by 10 (birthday money)
+// buyHamster(hamster) - push the hamster object onto the hamster array, increment mood by 10, decrement bankAccount by the value of the hamster (hint: use getPrice())
+// 🔴 Hard Mode Save & Commit your work!
+// Your commit message should read something like:
+// "created person class"
+// Easy Mode Keep Going and save and run code
+
+class Person {
+    constructor(name) {
+        this.name = name;
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = 0
+        this.hamster = []
+        this.bankAccount = 0
+    }
+    getName(){
+        return this.name
+    }
+    getAge(){
+        return this.age
+    }
+    getWeight(){
+        return this.weight
+    }
+    greet(){
+        console.log(this.name)
+    }
+    eat(){
+        this.weight++;this.mood++
+    }
+    exercise(){
+        this.weight--
+    }
+    ageUp(){
+        this.ageUp++;this.height++;this.weight++;this.mood--;
+       this.bankAccount += 10
+    }
+    buyHamster(hamster){
+        this.hamster.push(hamster);
+        this.mood +=10;
+        this.bankAccount -= hamster.getPrice
+    }
+
+}
